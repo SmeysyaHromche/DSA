816a2532eb54b3afa277b436f16bc9a302004c4a
SmeysyaHromche <MyrKykhta@gmail.com>
2024-07-10 20:49:21 +0200

    add general node class

--- DSA/List/ListNode.cs
+++ DSA/List/ListNode.cs
@@ -7,126 +7,18 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using DSA.Node;
 
 namespace DSA.List
 {
-    public class ListNode<T> where T : IComparable<T>
+    public class ListNode<T>: Node<T> where T: IComparable<T>
     {
-        protected T _value;
-        public T Value { get { return _value; } set { _value = value; } }
         public ListNode<T> next = null;
 
         public ListNode(T value)
         {
             _value = value;
         }
-
-        public override int GetHashCode()
-        {
-            return _value.GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-            {
-                return false;
-            }
-            else if (obj is ListNode<T> node)
-            {
-                return _value.CompareTo(node.Value) == 0;
-            }
-            else if (obj is T value)
-            {
-                return _value.Equals(value);
-            }
-            return false;
-        }
-
-        private static void CheckArg(ListNode<T> left, object right)
-        {
-            if (ReferenceEquals(left, null))
-            {
-                throw new ArgumentNullException(nameof(left));
-            }
-            if (right == null)
-            {
-                throw new ArgumentNullException(nameof(right));
-            }
-
-        }
-
-        public static bool operator ==(ListNode<T> left, ListNode<T> right)
-        {
-            if (ReferenceEquals(left, null))
-            {
-                return ReferenceEquals(right, null);
-            }
-            return left.Equals(right);
-
-        }
-        public static bool operator !=(ListNode<T> left, ListNode<T> right)
-        {
-            return !(left == right);
-        }
-
-        public static bool operator ==(ListNode<T> left, T right)
-        {
-            if (ReferenceEquals(left, null))
-            {
-                return false;
-            }
-            return left.Value.Equals(right);
-        }
-        public static bool operator !=(ListNode<T> left, T right)
-        {
-            return !(left == right);
-        }
-
-        public static bool operator <(ListNode<T> left, ListNode<T> right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right.Value) < 0;
-        }
-        public static bool operator >(ListNode<T> left, ListNode<T> right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right.Value) > 0;
-        }
-        public static bool operator <=(ListNode<T> left, ListNode<T> right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right.Value) <= 0;
-        }
-        public static bool operator >=(ListNode<T> left, ListNode<T> right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right.Value) > 0;
-        }
-        public static bool operator <(ListNode<T> left, T right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right) < 0;
-        }
-        public static bool operator >(ListNode<T> left, T right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right) > 0;
-        }
-        public static bool operator <=(ListNode<T> left, T right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right) <= 0;
-        }
-        public static bool operator >=(ListNode<T> left, T right)
-        {
-            CheckArg(left, right);
-            return left.Value.CompareTo(right) >= 0;
-        }
-
-
-
-
 
     }
 }
---
+++ DSA/Node/Node.cs
@@ -0,0 +1,119 @@
+﻿using DSA.List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Node
+{
+    public class Node<T> where T: IComparable<T>
+    {
+        protected T _value;
+        public T Value { get => _value; set => _value = value; }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            else if (obj is Node<T> node)
+            {
+                return _value.CompareTo(node.Value) == 0;
+            }
+            else if (obj is T value)
+            {
+                return _value.Equals(value);
+            }
+            return false;
+        }
+
+        protected static void CheckArg(Node<T> left, object right)
+        {
+            if (ReferenceEquals(left, null))
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+            if (right == null)
+            {
+                throw new ArgumentNullException(nameof(right));
+            }
+
+        }
+
+        public static bool operator ==(Node<T> left, Node<T> right)
+        {
+            if (ReferenceEquals(left, null))
+            {
+                return ReferenceEquals(right, null);
+            }
+            return left.Equals(right);
+
+        }
+        public static bool operator !=(Node<T> left, Node<T> right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(Node<T> left, T right)
+        {
+            if (ReferenceEquals(left, null))
+            {
+                return false;
+            }
+            return left.Value.Equals(right);
+        }
+        public static bool operator !=(Node<T> left, T right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(Node<T> left, Node<T> right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right.Value) < 0;
+        }
+        public static bool operator >(Node<T> left, Node<T> right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right.Value) > 0;
+        }
+        public static bool operator <=(Node<T> left, Node<T> right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right.Value) <= 0;
+        }
+        public static bool operator >=(Node<T> left, Node<T> right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right.Value) > 0;
+        }
+        public static bool operator <(Node<T> left, T right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right) < 0;
+        }
+        public static bool operator >(Node<T> left, T right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right) > 0;
+        }
+        public static bool operator <=(Node<T> left, T right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right) <= 0;
+        }
+        public static bool operator >=(Node<T> left, T right)
+        {
+            CheckArg(left, right);
+            return left.Value.CompareTo(right) >= 0;
+        }
+
+    }
+}
